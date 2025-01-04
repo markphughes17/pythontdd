d156c99dd897a130e286e87d2aa5eabaed89024d
markhughes <mark.hughes@filament.uk.com>
2025-01-04 13:18:28 +0000

    Refactor home page view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -6,8 +6,6 @@ from lists.views import home_page
 
 # Create your tests here.
 class HomePageTest(TestCase):
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         response = self.client.get("/")
-        self.assertContains(response, "<title>To-Do lists</title>")
-        self.assertContains(response, "<html>")
-        self.assertTrue(response, "</html>")
+        self.assertTemplateUsed(response, "home.html")
